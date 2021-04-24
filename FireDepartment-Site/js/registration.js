7683c13a983852f384da613c786f479f07533a43
Ivan Staev <IDStaev19@codingburgas.bg>
2021-04-24 13:29:48 +0300

    Add table for registered accidents

--- FireDepartment-Site/js/registration.js
+++ FireDepartment-Site/js/registration.js
@@ -30,3 +30,31 @@ function onSubmit() {
 
     localStorage.setItem('accidentsData', JSON.stringify(accidentsDataArray));
 }
+
+function showTableData() {
+    let table = document.getElementById('accidentsTable');
+    let alertEmptyAccidents = document.getElementById('alertEmptyAccidents');
+
+    if(JSON.parse(localStorage.getItem('accidentsData'))){
+        if (JSON.parse(localStorage.getItem('accidentsData')) && table) {
+            JSON.parse(localStorage.getItem('accidentsData')).forEach((item) => {table.innerHTML += `<tr><td>${item.office}</td>
+                                                                                                     <td>${item.team}</td>
+                                                                                                     <td>${item.truck}</td>
+                                                                                                     <td>${item.location}</td>
+                                                                                                     <td>${item.address}</td>
+                                                                                                     <td>${item.office}</td></tr>`;});
+        }
+        if (alertEmptyAccidents) alertEmptyAccidents.style.display = 'none';
+    }
+    else {
+        accidentsDataArray = [];
+        if (alertEmptyAccidents) alertEmptyAccidents.style.display = 'block';
+    }
+}
+
+function clearData() {
+    localStorage.clear();
+    document.getElementById('accidentsTable').innerHTML = '';
+}
+
+showTableData();
