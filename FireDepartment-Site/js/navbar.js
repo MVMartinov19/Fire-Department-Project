c71f2a53af6adbe3532fc04cf0d632297de91c08
Teodora Kompanska <TNKompanska19@codingburgas.bg>
2021-04-26 23:04:09 +0300

    Add comments to navbar.js

--- FireDepartment-Site/js/navbar.js
+++ FireDepartment-Site/js/navbar.js
@@ -1,16 +1,23 @@
+// Initialization of exit mobile navbar button
 let navbarExitButton = document.getElementById('exit');
+//Make exit mobile button to not display
 navbarExitButton.style.display = 'none';
 
+//Function that runs when user clicks on button that shows the mobile navbar
 function navbarResponsive() {
     let navbar = document.getElementById('mobileNav');
+
+    /*The navbar and the exit button display*/
     navbar.style.display = 'block';
     navbarExitButton.style.display = 'block';
 
 }
 
+//Function that runs when user clicks on button that exits the mobile navbar
 function navbarExit() {
     let navbar = document.getElementById('mobileNav');
-    let navbarExitButton = document.getElementById('exit');
+
+    /* The navbar and the exit button don't display */
     navbarExitButton.style.display = 'none';
     navbar.style.display = 'none';
 
