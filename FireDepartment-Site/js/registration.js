f6c910d32f650875cfcd8aa5f03827383fa17cdc
Teodora Kompanska <TNKompanska19@codingburgas.bg>
2021-04-27 06:16:47 +0300

    Update README.md. Fix #4

--- FireDepartment-Site/js/registration.js
+++ FireDepartment-Site/js/registration.js
@@ -1,3 +1,4 @@
+//initialize an empty object for a single incident's data
 let accidentData = {
     office: '',
     team: '',
@@ -7,20 +8,33 @@ let accidentData = {
     date: ''
 };
 
+//initialize an epmty array
 let accidentsDataArray = [];
 
-function checkIsAccidents() {
+//checks wheather there is any data submitted by the user
+function checkIsAccidents()
+{
     const alertEmptyAccidents = document.getElementById('alertEmptyAccidents');
-    if (localStorage.getItem('accidentsData')) {
+
+    //checks wheather there is accidents data in the local storage
+    if (localStorage.getItem('accidentsData'))
+    {
+        //hides the message indicating theer is no data
         accidentsDataArray = JSON.parse(localStorage.getItem('accidentsData'));
         if (alertEmptyAccidents) alertEmptyAccidents.style.display = 'none';
-    } else {
+    }
+    else
+    {
+        //shows the message indicating there is no data
         accidentsDataArray = [];
         if (alertEmptyAccidents) alertEmptyAccidents.style.display = 'block';
     }
 }
 
-function onSubmit() {
+//stores data in local storage
+function onSubmit()
+{
+    //assigns values from users's inputs to object
     accidentData = {
         office: document.getElementById('office').value,
         team: document.getElementById('team').value,
@@ -29,28 +43,42 @@ function onSubmit() {
         address: document.getElementById('address').value,
         date: document.getElementById('date').value
     };
+    //adds the object with user's values to the array
     accidentsDataArray.push(accidentData);
-    localStorage.setItem('accidentsData', JSON.stringify(accidentsDataArray)); // Превръщам дейтата в стринг за да се запази в локъл сторидж
+    //stringifies the array to set it to a value to the local storage by the key name "accidentsData"
+    localStorage.setItem('accidentsData', JSON.stringify(accidentsDataArray));
 }
 
-function showTableData() {
+
+//builds the table for the registered accidents
+function showTableData()
+{
     const table = document.getElementById('accidentsTable');
-    if (JSON.parse(localStorage.getItem('accidentsData')) && table) {
+    //if there is data
+    if (JSON.parse(localStorage.getItem('accidentsData')) && table)
+    {
         JSON.parse(localStorage.getItem('accidentsData')).forEach((item) => { table.innerHTML += `<tr><td>${item.office}</td>
                                                                                                      <td>${item.team}</td>
                                                                                                      <td>${item.truck}</td>
                                                                                                      <td>${item.location}</td>
                                                                                                      <td>${item.address}</td>
                                                                                                      <td>${item.date}</td></tr>`; });
-    } else {
+    }
+    else
+    {
+        //hides the table
         alertClearAccidents.style.display = 'none';
         alertEmptyAccidents.style.display = 'block';
         table.style.display = 'none';
     }
 }
 
-function clearData() {
+//clears the data
+function clearData()
+{
+    //clears the data from the local storage
     localStorage.clear();
+    //clears the data from the table
     document.getElementById('accidentsTable').innerHTML = '';
     alertClearAccidents.style.display = 'none';
     alertEmptyAccidents.style.display = 'block';
--- README.md
+++ README.md
@@ -46,3 +46,51 @@
 <p align="center">1. The main idea of our project is to make site that can help the firemans in the Burgas and its region. The goal is to give information about employees, accidents, fire trucks, fire extinguishers and also to make registration of an accident. </p>
 
 <p align="center"> <i> To make this site we have to use HTML&CSS&JavaScript. </i> </p>
+<hr>
+
+<h1 align="center"> 💫The realization of our project💫 </h1>
+
+<p align="center"> <b> First and the most important thing in this project is to make useful user-friendly site. </b> </p>
+<p align = "center">Personally our site went through 4 stages of design. Here they are:</p>
+<br>
+<h3>1. The design with orange-red range</h3>
+<img src="https://i.imgur.com/Ss1N8M4.png">
+
+<h3>2. The design with range of shades of blue</h3>
+<img src="https://i.imgur.com/fc21DBk.png">
+
+<h3>3. Totally new design with range of shades of black and gray</h3>
+<img src = "https://i.imgur.com/AnajgMs.png">
+
+<h3>4. The final result - instead of color range, we added backgrounds</h3>
+<img src = "https://i.imgur.com/Ab7TNxi.png">
+
+<hr>
+<br><br>
+<h1 align="center">🖥Interface🖥</h1>
+
+<h3>Home page</h3>
+<p> In home page we have firemans' motto and some information about their offices in Burgas</p>
+<img src="https://i.imgur.com/Ab7TNxi.png">
+
+<h3>Statistics' pages</h3>
+<p>They both look the same. but with different photos and information. In these pages we have cards of the most bad accidends and the most participated employees in accidents. In the end we have table with all the information.</p>
+
+<img src="https://i.imgur.com/KZoi8p5.png">
+
+<h3>Employees' page</h3>
+<p>In this page we have all the information about the employees and the firemans' teams. Have in mind that th different account image are for different official positions or different teams.</p>
+<img src="https://i.imgur.com/wa6vAZR.png">
+
+<h3>Pages for information</h3>
+<p>These pages look the same too, but with different images and information. We have card about fire extinguishers info and fire trucks info. </p>
+<img src="https://i.imgur.com/pUAJQ2w.png">
+
+<h3>Registration and registered accidents</h3>
+<p>We also made an accident registration, but this page we leave it to you to examine it. When we have registered accidents they will appear in registered accidents page in the form of table. Have in mind that when we don't have a registered accident, a message will appear.<p>
+ <img src="https://i.imgur.com/VFVPKRJ.png">
+ 
+ <hr>
+ 
+ <h1 align = "center"><b><i>💫 Hope you enjoy it!!! 💫</i></b></h1>
+
