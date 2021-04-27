1352dc99bc4d627aadc131b5b85f7751a30ee831
Ivan Staev <IDStaev19@codingburgas.bg>
2021-04-27 18:59:57 +0300

    Fix code indentation

--- FireDepartment-Site/js/navbar.js
+++ FireDepartment-Site/js/navbar.js
@@ -4,21 +4,21 @@ let navbarExitButton = document.getElementById('exit');
 navbarExitButton.style.display = 'none';
 
 //Function that runs when user clicks on button that shows the mobile navbar
-function navbarResponsive() {
+function navbarResponsive()
+{
     let navbar = document.getElementById('mobileNav');
 
     /*The navbar and the exit button display*/
     navbar.style.display = 'block';
     navbarExitButton.style.display = 'block';
-
 }
 
 //Function that runs when user clicks on button that exits the mobile navbar
-function navbarExit() {
+function navbarExit()
+{
     let navbar = document.getElementById('mobileNav');
 
     /* The navbar and the exit button don't display */
     navbarExitButton.style.display = 'none';
     navbar.style.display = 'none';
-
 }
